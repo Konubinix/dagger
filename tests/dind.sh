21b86041b690920f606c6bea584f42433498c59b
Samuel Loury <konubinixweb@gmail.com>
2026-04-03 23:59:50 +0200

    fix: test something more reproducible

--- tests/dind.sh
+++ tests/dind.sh
@@ -23,71 +23,12 @@ exit 1
 
 
 dind_docker_info_code () {
-      dagger call dind-with-docker --cmd="docker info" stdout
+      dagger call dind-with-docker --cmd="docker info --format '{{.OSType}}'" stdout
 }
 
 dind_docker_info_expected () {
       cat<<"EOEXPECTED"
-  Client: Docker Engine - Community
-   Version:    29.3.1
-   Context:    default
-   Debug Mode: false
-   Plugins:
-    buildx: Docker Buildx (Docker Inc.)
-      Version:  v0.33.0
-      Path:     /usr/libexec/docker/cli-plugins/docker-buildx
-    compose: Docker Compose (Docker Inc.)
-      Version:  v5.1.1
-      Path:     /usr/libexec/docker/cli-plugins/docker-compose
-
-  Server:
-   Containers: 0
-    Running: 0
-    Paused: 0
-    Stopped: 0
-   Images: 0
-   Server Version: 29.3.1
-   Storage Driver: overlayfs
-    driver-type: io.containerd.snapshotter.v1
-   Logging Driver: json-file
-   Cgroup Driver: cgroupfs
-   Cgroup Version: 2
-   Plugins:
-    Volume: local
-    Network: bridge host ipvlan macvlan null overlay
-    Log: awslogs fluentd gcplogs gelf journald json-file local splunk syslog
-   CDI spec directories:
-    /etc/cdi
-    /var/run/cdi
-   Swarm: inactive
-   Runtimes: io.containerd.runc.v2 runc
-   Default Runtime: runc
-   Init Binary: docker-init
-   containerd version: 301b2dac98f15c27117da5c8af12118a041a31d9
-   runc version: v1.3.4-0-gd6d73eb8
-   init version: de40ad0
-   Security Options:
-    seccomp
-     Profile: builtin
-    cgroupns
-   Kernel Version: 6.8.0-64-generic
-   Operating System: Ubuntu 24.04.4 LTS (containerized)
-   OSType: linux
-   Architecture: aarch64
-   CPUs: 8
-   Total Memory: 23.42GiB
-   Name: 1e50e5c6-67a3-46a1-8114-04252517c214
-   ID: 6efa0e05-37d1-4a37-96a0-be2299fd76bd
-   Docker Root Dir: /var/lib/docker
-   Debug Mode: false
-   Experimental: false
-   Insecure Registries:
-    ::1/128
-    127.0.0.0/8
-   Live Restore Enabled: false
-   Firewall Backend: iptables
-
-
+linux
 EOEXPECTED
 }
 
